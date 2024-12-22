3675909ee26baf08a32579259e3ee0db7c0d6a3e
Viktoria Markus <148903307+Viktoria1122@users.noreply.github.com>
2024-12-22 21:40:58 +0200

    Create FlowerstoreRebootApplicationTests.java

---
+++ src/test/java/FlowerstoreRebootApplicationTests.java
@@ -0,0 +1,13 @@
+package apps.ua.edu.ucu.flowerstore_reboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerstoreRebootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
